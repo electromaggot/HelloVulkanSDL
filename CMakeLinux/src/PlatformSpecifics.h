39e052a618c778bfe2f0bbef5f2e7c8c504481b3
electromaggot <electromaggot@gmail.com>
2023-07-17 01:27:57 -0500

    Add Linux CMake support as build platform.

---
+++ CMakeLinux/src/PlatformSpecifics.h
@@ -0,0 +1,21 @@
+//
+// PlatformSpecifics.h
+//	LINUX-CENTRIC
+//
+// Only make this file part of Linux/CMake projects.
+//
+// (Other versions should match, such as: VisualStudio/src/PlatformSpecifics.h, Xcode/src/PlatformSpecifics.h)
+//
+// Created 6/14/19 by Tadd Jensen
+//	© 0000 (uncopyrighted; use at will)
+//
+#ifndef PlatformSpecifics_h
+#define PlatformSpecifics_h
+
+
+#include <limits>
+
+// Nothing to see here.  Move along, move along.
+
+
+#endif // PlatformSpecifics_h
---
+++ src/shaders/compileLinux.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+COMPILER=/usr/bin/glslangValidator
+
+DSTDIR=../../CMakeLinux/compiledShaders
+
+
+for SRC in *.vert *.frag; do
+
+  OUT="$(echo "$SRC" | sed "s/\.frag$/-frag/" | sed "s/\.vert$/-vert/").spv"
+
+  # don't re-compile if existing binary is newer than source file
+
+  NEWER="$(ls -t1 "$SRC" "$OUT" | head -1)"
+
+  if [ "$SRC" = "$NEWER" ]; then
+
+    echo "COMPILING $OUT from:"
+
+    $COMPILER -V "$SRC" -o "$OUT"
+
+    # for Apple projects' benefit, copy the SPVs (vs. Xcode copying, not following, the symbolic link)
+
+    echo "copying to $DSTDIR"
+
+	cp "$OUT" "$DSTDIR"
+
+  else
+
+    echo "(unchanged $SRC)"
+
+  fi
+
+done
