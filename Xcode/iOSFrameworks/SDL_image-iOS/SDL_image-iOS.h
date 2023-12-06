04fad72ae2dfcb4d46abc1f882d5ea26809c93dc
Tadd Jensen <tadd@megaphonestudios.com>
2023-12-05 20:42:58 -0600

    Re-introduce script/subprojs to build Fat Frameworks, after mistaken notion SDL now builds XCFrameworks.

---
+++ Xcode/iOSFrameworks/SDL_image-iOS/SDL_image-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SDL_image-iOS.h
+//  SDL_image-iOS
+//
+//  Created by Tadd on 7/9/16.
+//  Copyright © 2016 Tadd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDL_image-iOS.
+FOUNDATION_EXPORT double SDL_iOSVersionNumber;
+
+//! Project version string for SDL_image-iOS.
+FOUNDATION_EXPORT const unsigned char SDL_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDL_image/PublicHeader.h>
+#import <../../External/SDL_image/SDL_image.h>
+
